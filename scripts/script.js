0bbc2953b364bbba9d19a8f97cd54c4f669959ce
carlo-fidel-taboada <carlofideltech@gmail.com>
2026-01-30 17:34:19 +0100

    fix-almost finish

--- scripts/script.js
+++ scripts/script.js
@@ -37,6 +37,9 @@ acordeon.forEach((tap) => {
 const listaFeatures = document.querySelectorAll(".featureList");
 const slideFeatureImg = document.querySelector(".slider-container");
 
+const listaFeaturesFirst = listaFeatures[0].querySelector(".elem-barra");
+listaFeaturesFirst.classList.add("barra-active");
+
 function cleanEventBar() {
   listaFeatures.forEach((item) => {
     const barra = item.querySelector(".elem-barra");
@@ -71,3 +74,63 @@ function handleClickCloseMenu() {
   menuSection.classList.remove("menu-open");
 }
 menuCloseBtn.addEventListener("click", handleClickCloseMenu);
+
+// ...existing code...
+
+/* ------------------------------------------------------------
+                          SLIDE 
+------------------------------------------------------------*/
+const btnAll = document.querySelectorAll(".featureList");
+const slide = document.querySelector(".slider");
+
+let current = "btn-01";
+
+btnAll.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const idElement = e.target.id;
+    const cases = `from ${current} to ${idElement}`;
+
+    if (idElement === current) return;
+    operateCases(cases);
+
+    current = idElement;
+  });
+});
+
+function operateCases(cases) {
+  switch (cases) {
+    case `from btn-01 to btn-02`:
+      cleanClasses();
+      slide.classList.add("back-One");
+      break;
+    case `from btn-01 to btn-03`:
+      cleanClasses();
+      slide.classList.add("foward-Two");
+      break;
+    case `from btn-02 to btn-03`:
+      cleanClasses();
+      slide.classList.add("foward-Two");
+      break;
+    case `from btn-02 to btn-01`:
+      cleanClasses();
+      slide.classList.add("foward-One");
+      break;
+    case `from btn-03 to btn-01`:
+      cleanClasses();
+      slide.classList.add("foward-One");
+      break;
+    case `from btn-03 to btn-02`:
+      cleanClasses();
+      slide.classList.add("back-One");
+      break;
+    default:
+      break;
+  }
+}
+
+function cleanClasses() {
+  slide.classList.remove("foward-One");
+  slide.classList.remove("foward-Two");
+  slide.classList.remove("back-One");
+  slide.classList.remove("back-Two");
+}
